e8fa74b92a00b2b5fa51c31d7ca655d5440d5028
sanjula pilapitiya <sanjulapilapitiya@hotmail.com>
2025-04-16 17:54:40 +0100

    fixed typo, reinstalled node files

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="py-3 my-4">
-        <p class="text-center text-light">Made by yours truely</p>
+        <p class="text-center text-light">Made by your's truly</p>
         <p class="text-center text-light">Made with Vue, Bootstrap and hosted on git pages</p>
     </footer>
 </template>
